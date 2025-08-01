dcbe3a4bb3eddef26d058bb2df2e22fad96fe74f
Jo Allam <joallam2008@gmail.com>
2025-08-02 01:28:37 +0300

    hopefully last dark mode edit - colors

--- js/app.js
+++ js/app.js
@@ -1,10 +1,6 @@
 let root = document.documentElement;
 let toggle = document.getElementById("switch");
-let oranges = document.getElementsByClassName('even');
 
 toggle.addEventListener("change", () => {
     root.classList.toggle('dark');
-    for (let orange of oranges) {
-        orange.style.color = 'black';
-    }
 });
